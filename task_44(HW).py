24e8cebd21f09081ca3d6ef20732fd7aa1e2c8c0
Roman_Konkov <roman.konkoff@gmail.com>
2024-02-13 18:35:07 +0400

    added comments for each step

--- task_44(HW).py
+++ task_44(HW).py
@@ -19,15 +19,18 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 
+# Создаем словарь, где каждый уникальный элемент списка будет ключом, а его индекс будет значением
 unique_values = {val: i for i, val in enumerate(set(lst))}
+# Инициализируем список списков для one-hot кодирования                                                            
 one_hot_encoded = []
+# Проходим по списку и создаем one-hot вектор для каждого элемента
 for item in lst:
     one_hot_vector = [0] * len(unique_values)
     index = unique_values[item]
     one_hot_vector[index] = 1
     one_hot_encoded.append(one_hot_vector)
-
+# Создаем DataFrame из one-hot векторов
 one_hot_df = pd.DataFrame(one_hot_encoded, columns=unique_values.keys())
-
+# Выводим на печать оба дата-фрейма
 print(f'{data.head()}\n\n')
 print(one_hot_df.head())
