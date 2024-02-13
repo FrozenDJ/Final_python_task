07d8c26a052637f9b5395588c22e1d08a7ba4f0c
Roman_Konkov <roman.konkoff@gmail.com>
2024-02-13 18:27:30 +0400

    added code to new branch

--- task_44(HW).py
+++ task_44(HW).py
@@ -11,3 +11,23 @@ data = pd.DataFrame({'whoAmI':lst})
 data.head()
 
 """
+
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+
+unique_values = {val: i for i, val in enumerate(set(lst))}
+one_hot_encoded = []
+for item in lst:
+    one_hot_vector = [0] * len(unique_values)
+    index = unique_values[item]
+    one_hot_vector[index] = 1
+    one_hot_encoded.append(one_hot_vector)
+
+one_hot_df = pd.DataFrame(one_hot_encoded, columns=unique_values.keys())
+
+print(f'{data.head()}\n\n')
+print(one_hot_df.head())
